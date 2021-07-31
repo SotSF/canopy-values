38dd4e6ca8637e00e17ae30f8363a8a4e9f13664
Jeff Wang <jeffawang@gmail.com>
2021-07-30 22:58:16 -0700

    add --public flag

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request, abort, render_template
 from flask_sqlalchemy import SQLAlchemy
+import argparse
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stuff.db'
@@ -58,4 +59,14 @@ if __name__ == '__main__':
     # create the db if it doesn't exist
     db.create_all()
     db.session.commit()
-    app.run(debug=True)
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--public", action='store_true')
+    args = parser.parse_args()
+
+    host=""
+    debug=True
+    if args.public:
+        host = '0.0.0.0'
+        debug=False
+    app.run(host=host, debug=False)
