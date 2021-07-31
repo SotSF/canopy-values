663e5631fad60d54c4aa54d6010bdfc243fbf4d1
Jeff Wang <jeffawang@gmail.com>
2021-07-30 22:47:59 -0700

    basic thingie

---
+++ main.py
@@ -0,0 +1,61 @@
+from flask import Flask, jsonify, request, abort, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stuff.db'
+db = SQLAlchemy(app)
+
+class Value(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    value = db.Column(db.Integer)
+
+    def json(self):
+        return {"name": self.name, "value": self.value}
+
+@app.route("/api/values", methods=["GET"])
+def integers():
+    a = Value.query.all()
+    print(a)
+    return jsonify([v.json() for v in a])
+
+@app.route("/api/values", methods=["POST"])
+def writeInteger():
+    print("writeInteger called:", request.json)
+    val = request.json
+    i = Value(name=val['name'], value=val['value'])
+    db.session.add(i)
+    db.session.commit()
+    return jsonify(i.json())
+
+@app.route("/api/values/<name>", methods=["PUT"])
+def update(name):
+    v = Value.query.filter_by(name=name).first()
+    if v is None:
+        return {"error": "not found", "code": 404}, 404
+    val = request.json
+    v.value = val['value']
+    db.session.add(v)
+    db.session.commit()
+    return jsonify(v.json())
+
+@app.route("/api/values/<name>")
+def getValue(name):
+    v = Value.query.filter_by(name=name).first()
+    if v is None:
+        return {"error": "not found", "code": 404}, 404
+    return jsonify(v.json())
+
+@app.route("/")
+def index():
+    return valuesIndex()
+
+@app.route("/values")
+def valuesIndex():
+    return render_template("values/index.html")
+
+if __name__ == '__main__':
+    # create the db if it doesn't exist
+    db.create_all()
+    db.session.commit()
+    app.run(debug=True)
---
+++ static/js/index.js
@@ -0,0 +1,69 @@
+cube = {rotation: {x: 0, y: 0, z: 0}}
+camera = {position: {z: 0}}
+
+var values = {};
+const gui = new dat.GUI();
+const folder = gui.addFolder('values')
+folder.open()
+
+function postValue(name, value) {
+    fetch(`/api/values`, {
+        method: "POST",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            name: name,
+            value: value
+        }
+        )
+    }).then(function() {location.reload(true)})
+}
+
+var addFieldValue = {
+    name: "",
+    value: 50,
+    butts: function(a){
+        postValue(this.name, this.value)
+    },
+    msg: ""
+}
+
+const addFieldFolder = gui.addFolder('new field')
+addFieldFolder.open()
+addFieldFolder.add(addFieldValue, 'name')
+addFieldFolder.add(addFieldValue, 'value', 0, 100)
+addFieldFolder.add(addFieldValue, 'butts').name("SUBMIT")
+
+
+function updateValue(name, value) {
+    fetch(`/api/values/${name}`, {
+        method: "PUT",
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            name: name,
+            value: value
+        }
+        )
+    })
+}
+
+function getValues() {
+    fetch('/api/values')
+        .then(response => response.json())
+        .then(function(vals) {
+            for (const val of vals) {
+                values[val.name] = val.value
+                controller = folder.add(values, val.name, 0, 100, 1)
+                controller.onChange(function(v) {
+                    updateValue(this.property, v)
+                })
+            }
+        })
+}
+
+function getValue(name) {
+    fetch(`/api/values/${name}`)
+        .then(response => response.json())
+        .then(val => console.log(val))
+}
+
+getValues()
