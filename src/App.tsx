dc0de264bcd0814cb73caee8fcec0e59b39bec82
Ben Rollin <ben.rollin@clever.com>
2022-06-25 16:32:38 -0700

    show position with blue circle

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 type KeyValue = {
@@ -6,33 +6,66 @@ type KeyValue = {
   value: number;
 };
 
-const loadValues = async () => {
+let dataStore: Record<string, number> = {};
+const fetchValues = async () => {
   const response = await fetch("/api/values");
-  const { values } = await response.json();
-  return values;
+  const values = (await response.json()).values as KeyValue[];
+  for (const { name, value } of values) {
+    dataStore[name] = value;
+  }
+  return dataStore;
 };
 
-const updateValue = async (name: string, value: number) =>
+const updateValue = async (name: string, value: number) => {
+  dataStore[name] = value;
   await fetch(`/api/values/${name}`, {
     method: "PUT",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ name, value }),
   });
-
-const updateValueDelta = async (name: string, delta: number) => {
-  const data: KeyValue[] = await loadValues();
-  const oldDatum = data.find((datum) => datum.name === name);
-  if (oldDatum) await updateValue(name, oldDatum.value + delta);
-  else console.log("cannot update missing key:", name);
 };
 
+const updateValueDelta = (name: string, delta: number) =>
+  updateValue(name, dataStore[name] + delta);
+
 function App() {
+  const [x, setX] = useState(50);
+  const [y, setY] = useState(50);
+
+  // Initialize x, y state with true values
+  const initializeData = async () => {
+    await fetchValues();
+    setX(dataStore.x);
+    setY(dataStore.y);
+  };
+  useEffect(() => {
+    initializeData();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <button onClick={() => updateValueDelta("y", 1)}>Up</button>
-        <button onClick={() => updateValueDelta("y", -1)}>Down</button>
+        <svg width="100%" height={500} viewBox={`0 0 100 100`}>
+          <g>
+            <circle r={5} cx={x} cy={y} fill="blue" />
+          </g>
+        </svg>
+        <button
+          onClick={() => {
+            setY(dataStore.y - 10);
+            updateValueDelta("y", -10);
+          }}
+        >
+          Up
+        </button>
+        <button
+          onClick={() => {
+            setY(dataStore.y + 10);
+            updateValueDelta("y", 10);
+          }}
+        >
+          Down
+        </button>
       </header>
     </div>
   );
